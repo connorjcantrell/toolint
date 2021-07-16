2aad2909063bbc34aeeebb4b51e72e81ff1ff54f
Connor Cantrell <connorjcantrell@gmail.com>
2021-07-16 00:04:30 -0700

    Created main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/connorjcantrell/toolint/postgres"
+	"github.com/connorjcantrell/toolint/web"
+)
+
+func main() {
+	// dsn := os.Getenv("URL_DB")
+	dsn := "postgres://root:secret@localhost/toolint?sslmode=disable"
+
+	store, err := postgres.NewStore(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sessions, err := web.NewSessionManager(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csrfKey := []byte("01234567890123456789012345678901")
+	h := web.NewHandler(store, sessions, csrfKey)
+	http.ListenAndServe(":3000", h)
 }
